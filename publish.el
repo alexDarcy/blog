62e1f98fb4a4e5b4ce6a117a0698532ab48615bd
Alexis Praga <alexis.praga@free.fr>
2021-04-22 18:09:27 +0200

    Beginning conversion to org mode for all posts

--- publish.el
+++ publish.el
@@ -7,9 +7,21 @@
 (require 'ox-publish)
 (require 'ox-html)
 
+;; Does not work
+(setq org-html-postamble-format
+      "<p class=\"author\">Author: %a (%e)</p>
+<p class=\"date\">Last updated: %C</p>
+<p class=\"date\">Created: %d</p>
+<p class=\"validation\">%v</p>")
+
+;; This works but we need more info
+;; (defun my-org-html-postamble (plist)
+ ;; (format "Last update : %s" (format-time-string "%d %b %Y")))
+(setq org-html-postamble 'my-org-html-postamble)
 ;;; Code:
 (setq org-html-html5-fancy t
       org-html-doctype "html5")
+
 
 ;; ;; org-html-head blog-html-head);; Can't set it in org-publish-project-alist...
 (setq org-publish-project-alist
@@ -20,6 +32,8 @@
          :publishing-directory "~/projects/blog/html/"
          :recursive t
          :with-toc nil
+         :with-author "Alexis Praga"
+         :with-creator nil
          :exclude "notes"
          ;; Custom CSS
          :html-style nil
@@ -31,5 +45,6 @@
          :publishing-function org-html-publish-to-html
          :headline-levels 4             ; Just the default for this project.
          :auto-sitemap t
+         :sitemap-filename "index.org"
         )))
 (provide 'publish)
